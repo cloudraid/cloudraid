8917cf730899f6f40a96fb1954b2d4c43fbc8256
Patrick Gansterer <paroga@paroga.com>
2014-10-27 00:38:27 +0100

    Add StorageManager files

--- routes/files.js
+++ routes/files.js
@@ -1,8 +1,13 @@
+var StorageManagerFactory = require('../lib/StorageManagerFactory');
 var express = require('express');
 var router = express.Router();
 
 router.get('/', function(req, res) {
-  res.render('files');
+  StorageManagerFactory.create(req.session.data.settings, function(err, storageManager) {
+    res.render('files', {
+      storageManager: storageManager
+    });
+  });
 });
 
 module.exports = router;
