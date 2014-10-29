e85ec1ccd444fe8aca4876ec96891a74c338c7af
Jan Sedlmayr <jan.sedlmayr@aon.at>
2014-10-29 22:23:02 +0100

    t Please enter the commit message for your changes. Lines starting yxcv
    cvjs
    
     wief modified:   routes/files.js
    	modified:   views/files.jade
    
     Untracked files:
       (use "git add <file>..." to include in what will be committed)
    
    	.idea/
    	files
    	npm-debug.log
    	settings
    t
    
    	
    	
    	  ccxy
    	   scy 
    sers

--- routes/files.js
+++ routes/files.js
@@ -1,12 +1,33 @@
 var StorageManagerFactory = require('../lib/StorageManagerFactory');
 var express = require('express');
 var router = express.Router();
+var storManager = null;
 
 router.get('/', function(req, res) {
   StorageManagerFactory.create(req.session.data.settings, function(err, storageManager) {
-    res.render('files', {
-      storageManager: storageManager
+    storManager = storageManager;
+    storageManager.getFileList(function(err, filelist) {
+      console.log('Get file list:' + filelist);
+      res.render('files', {
+        storageManager: storageManager,
+        files: filelist
+      });
     });
+
+  });
+});
+
+router.post('/create', function(req, res) {
+  console.log("Create file");
+  storManager.createFile(req.body.filename, req.body.file, function(err) {
+    if (err) {
+      res.render('error', {
+        message: err,
+        error: {}
+      });
+    } else {
+      res.redirect('/files');
+    }
   });
 });
 
